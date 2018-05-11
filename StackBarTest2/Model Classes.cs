c5f6576515b152cc291bf84735e8f5ad66c547a7
yuriy.deomidov <yuriy.deomidov@exquance.com>
2018-05-11 18:36:17 +0300

    Add basic classes

---
+++ StackBarTest2/Model Classes.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Markup;
+using System.Windows.Media;
+
+namespace StackBarTest2
+{
+    public class Floor
+    {
+        public string FullName
+        {
+            get { return this.ToString(); }
+        }
+        public Floor(string name)
+        {
+            Name = name;
+            Rooms = new ObservableCollection<Room>();
+        }
+        public string Name { get; }
+        public ObservableCollection<Room> Rooms { get; }
+        public double Area
+        {
+            get { return Rooms.Sum(x => x.Area); }
+        }
+
+        //public double TrueArea
+        //{
+        //    get { return Rooms.Where(x => x.Type != RoomType.Fake).Sum(x => x.Area); }
+        //}
+    }
+
+    public enum RoomType
+    {
+        Office = 0,
+        Vacant = 1,
+        Residential = 2,
+        Storage = 3
+    }
+
+    public class Room
+    {
+        public Room(string name, double area, RoomType type)
+        {
+            Name = name;
+            Area = area;
+            Type = type;
+        }
+        public string Name { get; }
+        public double Area { get; }
+
+        public RoomType Type { get; }
+    }
+}
---
+++ StackBarTest2/ViewModel.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace StackBarTest2
+{
+    public class ViewModel
+    {
+        public ViewModel()
+        {
+            PopulateData();
+
+        }
+
+        private void PopulateData()
+        {
+            _floors = new ObservableCollection<Floor>();
+            for (int i = 4; i > 0; i--)
+            {
+                Floor floor = new Floor($"floor{i}");
+                for (int j = 1; j < 8 - i; j++)
+                {
+                    Room room = new Room($"room{i}{j}", (i * 95.5 * j % 7) * 10.0, (RoomType)((i + j) % 4));
+                    floor.Rooms.Add(room);
+                }
+                _floors.Add(floor);
+            }
+
+            LegendDictionary = new Dictionary<string, Color>();
+            LegendDictionary.Add("test", Colors.Red);
+            LegendDictionary.Add("test2", Colors.Blue);
+            LegendDictionary.Add("test3", Colors.Green);
+
+        }
+
+        public Dictionary<string, Color> LegendDictionary { get; set; }
+
+        private ObservableCollection<Floor> _floors;
+        public ObservableCollection<Floor> Floors { get { return _floors; } }
+    }
+}
